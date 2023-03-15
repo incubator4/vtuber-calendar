83fe2f32c0bfd159d30361b03bebcb67cd58c6d4
incubator4 <aries0robin@gmail.com>
2023-03-15 19:46:31 +0800

    fix web

---
+++ web/src/views/vtuber/Milestone.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import moment from "moment";
+import { useVtuberStore } from "@/stores";
+const props = defineProps<{ vid: number }>();
+const store = useVtuberStore();
+store.listMilestones(props.vid);
+</script>
+<template>
+  <el-timeline>
+    <el-timeline-item
+      v-for="m in store.milestones"
+      :timestamp="moment(m.date).format('YYYY-MM-DD')"
+      placement="top"
+    >
+      <el-card>
+        <h4>{{ m.event }}</h4>
+        <p>{{ m.description }}</p>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
+</template>
